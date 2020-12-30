5b3e39f28d5eebda5edc3531e63a902291264346
Coline Brault <coline.brault@gmail.com>
2020-12-30 19:57:44 +0100

    contact form mailer functionning with regular email addresses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'coline.brault@gmail.com'
   layout 'mailer'
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,10 @@ class MessageMailer < ApplicationMailer
 
   def contact(message)
     @body = message.body
-    mail(to: 'coline.brault@gmail.com', subject: 'Nouveau message !')
+    @email = message.email
+    @phone_number = message.phone_number
+    @name = message.name
+    mail(to: 'coline.brault@gmail.com', subject: 'Nouveau message sur le site !')
   end 
+  
 end
