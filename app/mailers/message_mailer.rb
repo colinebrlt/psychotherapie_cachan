01d552bc15eff678ffd45b2dd738023b7754dfcf
Coline Brault <coline.brault@gmail.com>
2021-01-11 20:06:50 +0100

    helene's email address

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,7 @@ class MessageMailer < ApplicationMailer
     @email = message.email
     @phone_number = message.phone_number
     @name = message.name
-    mail(to: 'stefcallens@gmail.com', subject: 'Nouveau message sur le site !')
+    mail(to: 'helene.levy1@free.fr', subject: 'Nouveau message sur le site !')
   end 
 
 end
